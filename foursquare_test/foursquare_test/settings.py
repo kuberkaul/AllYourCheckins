dcc47563ef4ea84b25835fcee4b70ac05a7451d6
Kuber Kaul <kuberkaul@icloud.com>
2013-10-28 23:47:40 -0400

    added admin page on /admin/ with username,password = allyourcheckins

--- foursquare_test/foursquare_test/settings.py
+++ foursquare_test/foursquare_test/settings.py
@@ -12,7 +12,7 @@ MANAGERS = ADMINS
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': '/Users/digvijaysingh/allyourcheckins/foursquare_test/fq.db',                      # Or path to database file if using sqlite3.
+        'NAME': '../foursquare_test/fq.db',                      # Or path to database file if using sqlite3.
         # The following settings are not used with sqlite3:
         'USER': '',
         'PASSWORD': '',
@@ -111,7 +111,7 @@ TEMPLATE_DIRS = (
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
-    "/Users/digvijaysingh/allyourcheckins/foursquare_templates",
+    "../foursquare_templates",
 )
 
 INSTALLED_APPS = (
@@ -122,9 +122,9 @@ INSTALLED_APPS = (
     'django.contrib.messages',
     'django.contrib.staticfiles',
     # Uncomment the next line to enable the admin:
-    # 'django.contrib.admin',
+     'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
-    # 'django.contrib.admindocs',
+     'django.contrib.admindocs',
 )
 
 SESSION_SERIALIZER = 'django.contrib.sessions.serializers.JSONSerializer'
